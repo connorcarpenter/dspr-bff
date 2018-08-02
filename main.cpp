1c1c79ce8db222b3e3a66f8c37341be3d94f41a2
connorcarpenter <connorcarpenter@gmail.com>
2018-08-02 12:35:28 -0600

    bff gives client a gameserver and player-specific password

--- main.cpp
+++ main.cpp
@@ -13,16 +13,17 @@ int main()
     h.onMessage([&h](uWS::WebSocket<uWS::SERVER> *ws, char *data, size_t length, uWS::OpCode opCode)
     {
         if (strncmp(data, "gameservers/1.0/join", 20) == 0){
-            ws->send("gameserver/connect|www.deuspora.com:3173\r\n", uWS::BINARY); //the \r\n makes it happen on EMSC! Don't remove!
+            ws->send("gameserver/connect|www.deuspora.com:3173|player1code\r\n", uWS::BINARY); //the \r\n makes it happen on EMSC! Don't remove!
+            std::cout << "dspr-bff: Received 'gameservers/1.0/join', Sent 'gameserver/connect|www.deuspora.com:3173|player1code'" << std::endl;
         }
 
     });
 
     h.getDefaultGroup<uWS::SERVER>().startAutoPing(30000);
     if (h.listen("localhost", 3172)) {
-        std::cout << "Listening to port 3172" << std::endl;
+        std::cout << "dspr-bff: Listening to port 3172" << std::endl;
     } else {
-        std::cerr << "Failed to listen to port" << std::endl;
+        std::cerr << "dspr-bff: Failed to listen to port" << std::endl;
         return -1;
     }
 
